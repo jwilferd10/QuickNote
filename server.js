40ce661c2f26419dafc0c8f651b4162cba221074
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-07-27 20:31:26 -0700

    filterByQuery, findByID, createNewNotes, validateNote methods have been commented out and moved into notes.js, future adjustments will see these methods refactored.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
+const path = require('path');
+
 const { notes } = require('./data/notes');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require('path')
-
 
 // Middleware to parse incoming string or array data 
 app.use(express.urlencoded({ extended: true }));
@@ -14,45 +14,45 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-function filterByQuery(query, notesArray) {
-    let filteredResults = notesArray;
-    if (query.title) {
-        filteredResults = filteredResults.filter(notes => notes.title === query.title);        
-    }
-    if (query.text) {
-        filteredResults = filteredResults.filter(notes => notes.text === query.text);
-    }
-    // return the filtered results:
-    return filteredResults;
-}
+// function filterByQuery(query, notesArray) {
+//     let filteredResults = notesArray;
+//     if (query.title) {
+//         filteredResults = filteredResults.filter(notes => notes.title === query.title);        
+//     }
+//     if (query.text) {
+//         filteredResults = filteredResults.filter(notes => notes.text === query.text);
+//     }
+//     // return the filtered results:
+//     return filteredResults;
+// }
 
-function findByID(id, notesArray) {
-    const result = notesArray.filter(notes => notes.id === id)[0];
-    return result;
-}
+// function findByID(id, notesArray) {
+//     const result = notesArray.filter(notes => notes.id === id)[0];
+//     return result;
+// }
 
-// Create new notes
-function createNewNotes(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './data/notes.json'),
-        JSON.stringify({ notes: notesArray}, null, 2)
-    );
-    // return finished code to post route for response
-    return note;
-}
+// // Create new notes
+// function createNewNotes(body, notesArray) {
+//     const note = body;
+//     notesArray.push(note);
+//     fs.writeFileSync(
+//         path.join(__dirname, './data/notes.json'),
+//         JSON.stringify({ notes: notesArray}, null, 2)
+//     );
+//     // return finished code to post route for response
+//     return note;
+// }
 
-// Check if each key not only exists, but that it is also the right type of data.
-function validateNote(note) {
-    if (!note.title || typeof note.title !== 'string') {
-        return false;
-    }
-    if (!note.text || typeof note.text !== 'string') {
-        return false;
-    }
-    return true;
-}
+// // Check if each key not only exists, but that it is also the right type of data.
+// function validateNote(note) {
+//     if (!note.title || typeof note.title !== 'string') {
+//         return false;
+//     }
+//     if (!note.text || typeof note.text !== 'string') {
+//         return false;
+//     }
+//     return true;
+// }
 
 app.get('/api/notes', (req, res) => {
     let results = notes;
