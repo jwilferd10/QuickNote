46a6da44701cddfba70009eb6a111ef801699c07
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-07-28 23:28:45 -0700

    Removed commented logic from server.js

--- server.js
+++ server.js
@@ -14,46 +14,6 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-// function filterByQuery(query, notesArray) {
-//     let filteredResults = notesArray;
-//     if (query.title) {
-//         filteredResults = filteredResults.filter(notes => notes.title === query.title);        
-//     }
-//     if (query.text) {
-//         filteredResults = filteredResults.filter(notes => notes.text === query.text);
-//     }
-//     // return the filtered results:
-//     return filteredResults;
-// }
-
-// function findByID(id, notesArray) {
-//     const result = notesArray.filter(notes => notes.id === id)[0];
-//     return result;
-// }
-
-// // Create new notes
-// function createNewNotes(body, notesArray) {
-//     const note = body;
-//     notesArray.push(note);
-//     fs.writeFileSync(
-//         path.join(__dirname, './data/notes.json'),
-//         JSON.stringify({ notes: notesArray}, null, 2)
-//     );
-//     // return finished code to post route for response
-//     return note;
-// }
-
-// // Check if each key not only exists, but that it is also the right type of data.
-// function validateNote(note) {
-//     if (!note.title || typeof note.title !== 'string') {
-//         return false;
-//     }
-//     if (!note.text || typeof note.text !== 'string') {
-//         return false;
-//     }
-//     return true;
-// }
-
 app.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
