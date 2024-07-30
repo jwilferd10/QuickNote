0b8a9a7e64922293353b8b0627a2806322ef88a5
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-07-29 21:34:42 -0700

    Wrote a test to check if the new lib/notes.js file and the repository imports are working. The current test uses findByID to successfully query by ID.

---
+++ __tests__/notes.test.js
@@ -0,0 +1,21 @@
+import { filterByQuery, findByID, createNewNotes, validateNote } from '../lib/notes.js';
+
+// Use findByID to locate specific entries saved into the array
+test('Locate note by ID', () => {
+    const exampleNotes = [
+        {
+            id: "1",
+            title: "Remember to do something",
+            text:"But good luck trying to remember, WITHOUT NOTES!"
+        },
+        {
+            id: "2",
+            title: "Hello World",
+            text:"Hi hi hi!"
+        }
+    ]
+
+    // Search for ID 2 from our local array, expect title to match
+    const result = findByID('2', exampleNotes);
+    expect(result.title).toBe('Hello World');
+});
